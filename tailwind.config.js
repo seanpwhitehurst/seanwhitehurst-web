4522272cf70771a28160c56ab4219372b4e04626
Sean Whitehurst <spwhitehurst14@gmail.com>
2021-10-01 21:06:42 -0700

    fonts and shit and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], // basic tree shaking
   darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#1DB0FF',
+          light: '#C2EAFF',
+          dark: '#1A5777',
+        },
+        surface: {
+          DEFAULT: '#FFFFFF',
+          dark: '#303136',
+        },
+        background: {
+          DEFAULT: '#156064',
+        },
+        words: {
+          DEFAULT: '#000',
+          dark: '#CCC',
+          sub: '#BEC0C5'
+        }
+      },
+      fontFamily: {
+        app: ['Roboto Mono']
+      },
+    },
   },
   variants: {
     extend: {},

5e7a3e6f69ab189dd638663802f5a4d3459615c5
Sean Whitehurst <spwhitehurst14@gmail.com>
2021-10-01 20:50:58 -0700

    more config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDJOcDj6uVoJiO02D1I40Zsh6dPGudBHEU",
+  authDomain: "seanwhitehurst-core.firebaseapp.com",
+  projectId: "seanwhitehurst-core",
+  storageBucket: "seanwhitehurst-core.appspot.com",
+  messagingSenderId: "749533092785",
+  appId: "1:749533092785:web:1b7b764ecba03ef0367b90",
+  measurementId: "G-SMG4JDG0H0"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
